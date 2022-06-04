a6f491374f7e24c7f2ce45183beefbc0c3120827
Piotr Tynek <tynek.piotr@gmail.com>
2022-06-04 15:18:18 +0200

    Task 14.1

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.spring.intro.shape;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class DrawerTestSuite{
+
+    @Test
+    void testDoDrawingWithCircle(){
+        Circle circle = new Circle();
+
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+
+        assertEquals("This is a circle", result);
+    }
+
+    @Test
+    void testDoDrawingWithTriangle(){
+
+        Triangle triangle = new Triangle();
+
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+
+        assertEquals("This is a triangle", result);
+    }
+}
