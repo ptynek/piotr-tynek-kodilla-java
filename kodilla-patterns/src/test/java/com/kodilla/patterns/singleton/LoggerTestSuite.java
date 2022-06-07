e102e844e8e0b2133fb924fe9764060c6a37c440
Piotr Tynek <tynek.piotr@gmail.com>
2022-06-07 21:01:21 +0200

    Task 15.1

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.patterns.singleton;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +9,19 @@ public class LoggerTestSuite {
 
     private static Logger logger;
 
+    @BeforeAll
+    public static void setLog() {
+        logger = Logger.INSTANCE;
+        logger.log("Logger check");
+
+    }
+
     @Test
     void testGetLastLog(){
 
+
+        String result = logger.getLastLog();
+
+        assertEquals("Logger check", result);
     }
 }
