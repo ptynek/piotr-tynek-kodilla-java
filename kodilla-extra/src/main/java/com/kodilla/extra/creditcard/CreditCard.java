2dddedb32e82483a86ea10cd6ecf00f25383df9a
Piotr Tynek <tynek.piotr@gmail.com>
2022-06-14 19:48:41 +0200

    Fixed extra task

--- kodilla-extra/src/main/java/com/kodilla/extra/creditcard/CreditCard.java
+++ kodilla-extra/src/main/java/com/kodilla/extra/creditcard/CreditCard.java
@@ -3,7 +3,7 @@ package com.kodilla.extra.creditcard;
 public class CreditCard {
     public String maskify(String ccNumber) {
 
-        if (ccNumber =="" || ccNumber == null){
+        if (ccNumber == null || ccNumber.equals("")){
             return "";
         } else if (ccNumber.length() > 6 && !ccNumber.matches(".*[a-z].*")){
             String toMask = ccNumber.substring(1, ccNumber.length()-4);
